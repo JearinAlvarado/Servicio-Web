498259059b614632e467d336c89800a68183b6a2
jearinAlvarado <jearinalvarado110403@gmail.com>
2023-03-31 17:02:09 -0400

    Se agrega el proyecto

--- App.js
+++ App.js
@@ -15,22 +15,22 @@ app.get('/add', (req, res)=>{
 });
 
 app.get('/cap.js', (req, res)=>{
-    res.sendFile(path.join(__dirname +'/Js/cap.js'));
+    res.sendFile(path.join(__dirname +'/Java/cap.js'));
 });
 
 app.get('/search.js', (req, res)=>{
-    res.sendFile(path.join(__dirname +'/Js/search.js'));
+    res.sendFile(path.join(__dirname +'/Java/search.js'));
 });
 
 app.get('/pull.js', (req, res)=>{
-    res.sendFile(path.join(__dirname +'/Js/pull.js'));
+    res.sendFile(path.join(__dirname +'/Java/pull.js'));
 });
 
 app.get('/fullList', (req, res)=>{
     res.sendFile(path.join(__dirname +'/Public/fullList.html'));
 });
 app.get('/function.js', (req, res)=>{
-    res.sendFile(path.join(__dirname +'/Js/function.js'));
+    res.sendFile(path.join(__dirname +'/Java/function.js'));
 
 })
 
