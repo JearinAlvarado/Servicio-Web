66c1b30297b21fbf684940ca6c23d34a14fb5d4c
jearinAlvarado <jearinalvarado110403@gmail.com>
2023-03-31 14:58:55 -0400

    commit

--- JS/Subir.js
+++ JS/Subir.js
@@ -2,7 +2,7 @@ function Subir(){
     fetch('http://www.raydelto.org/agenda.php')
     .then(response => response.json())
     .then(res=> {
-        return console.log('Subr check');
+        return console.log('Subir check');
 
     })
 }
